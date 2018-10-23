36a850391750e6e4cc30995ccd713217e7886510
sensdetection <antoniodonvito3@gmail.com>
2018-10-23 17:05:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,26 +22,22 @@ if($method == 'POST'){
 	switch ($text) {
 		case 'hi':
 			$speech = "Hi, Nice to meet you";
-			stampaMessaggio();
 			break;
 
 		case 'bye':
 			$speech = "Bye, good night";
-			stampaMessaggio();
 			break;
 		
 		case 'news':
-			for ($x = 0; $x <= 10; $x++) {
-			    	$speech = "belle news";
-				stampaMessaggio();
-			} 
+			$speech = "belle news";
 			break;
 		
 		default:
 			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			stampaMessaggio();
 			break;
 	}
+	
+	stampaMessaggio();
 
 }
 else
