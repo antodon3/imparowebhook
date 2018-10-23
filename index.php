5d2d141b668d8fa94c63b82d75a19752c5c64df6
sensdetection <antoniodonvito3@gmail.com>
2018-10-23 16:51:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php 
-require 'simple_html_dom.php';
 
 $siti = [];
 $notizie = [];
 $method = $_SERVER['REQUEST_METHOD'];
-
-$html = file_get_html('http://www.comune.barletta.bt.it/retecivica/avvisi18.htm');
-foreach($html->find('#bordovideo-112') as $item)
-{
-    // Find all <td> in <tr> 
-    foreach($item->find('tr') as $tr) 
-    {
-        foreach($tr->find('td') as $news) 
-        {
-            $notizie = $news->innertext;
-        }
-        foreach($tr->find('a') as $link) 
-        {
-            $sito = "http://www.comune.barletta.bt.it/retecivica/".$link->href;
-        }
-    }
-}
 
 // Process only when method is POST
 if($method == 'POST'){
@@ -39,7 +21,7 @@ if($method == 'POST'){
 			break;
 		
 		case 'news':
-			$speech = $notizie[1].$notizie[2];
+			$speech = "belle news";
 			break;
 		
 		default:
