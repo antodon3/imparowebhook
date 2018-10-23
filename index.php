062b6c9902a03a26efd1c0aaab28cb476ccbe08e
sensdetection <antoniodonvito3@gmail.com>
2018-10-23 16:18:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php 
+require 'simple_html_dom.php';
 
+$siti = [];
+$notizie = [];
 $method = $_SERVER['REQUEST_METHOD'];
+
+function scrapingNews() {
+	$html = file_get_html('http://www.comune.barletta.bt.it/retecivica/avvisi18.htm');
+	foreach($html->find('#bordovideo-112') as $item)
+	{
+	    // Find all <td> in <tr> 
+	    foreach($item->find('tr') as $tr) 
+	    {
+		foreach($tr->find('td') as $news) 
+		{
+		    echo "tabella";
+		    print_r($news->innertext);
+		    $notizie = $news->innertext;
+		}
+		foreach($tr->find('a') as $link) 
+		{
+		    echo "sito";
+		    print_r("http://www.comune.barletta.bt.it/retecivica/".$link->href);
+		    $sito = "http://www.comune.barletta.bt.it/retecivica/".$link->href;
+		}
+	    }
+	}
+}
 
 // Process only when method is POST
 if($method == 'POST'){
@@ -18,8 +44,16 @@ if($method == 'POST'){
 			$speech = "Bye, good night";
 			break;
 
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
+		case 'news':
+			for ($x = 0; $x < 5; $x++) {
+				$speech = $news[1].'\n'.$news[2];
+				$response = new \stdClass();
+
+				$response->speech = $speech;
+				$response->displayText = $speech;
+				$response->source = "webhook";
+				echo json_encode($response);
+			}
 			break;
 		
 		default:
